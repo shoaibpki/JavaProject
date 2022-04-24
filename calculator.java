aae250b69214d258ba0d66de1b63a0f4adc5b0cb
Shoaib Raza <shoaib_pki@hotmail.com>
2022-04-24 03:46:09 +0100

    update program

--- calculator.java
+++ calculator.java
@@ -3,13 +3,44 @@ package dataTypes;
 import java.util.Scanner;
 
 class operation {
-	void operations() {
+	void add(int num1, int num2) {
+		int addition = num1+num2;
+		System.out.println("the addition result is: " + addition);
+	}
+	void sub(int num1, int num2) {
+		int Subtraction = num1-num2;
+		System.out.println("the Subtraction result is: " + Subtraction);
+	}
+	void mul(int num1, int num2) {
+		int m = num1*num2;
+		System.out.println("the multiplication result is: " + m);
+	}
+	void div(int num1, int num2) {
+		int d = num1/num2;
+		System.out.println("the division result is: " + d);
+	}
+	void perc (int num1, int num2) {
+		int per = (num1+num2);
+		System.out.println("The percentage result is: " + per);
+	}
+	void mod(int num1, int num2) {
+		int m = num1%num2;
+		System.out.println("the modulus result is: " + m);
+	}
+}
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
+
 		System.out.println("enter the num 1");
 		int num1 = sc.nextInt();
+		
 		System.out.println("enter the num 2");
 		int num2 = sc.nextInt();
-		
+
 		System.out.println("enter operator \n"
 				+ "1. Addition\n"
 				+ "2. Subtraction\n"
@@ -19,44 +50,28 @@ class operation {
 				+ "6. percentage\n"
 				+ "7. exit");
 		int n = sc.nextInt();
+		operation op = new operation();
 		
 		if(n==1) {
-			int addition = num1+num2;
-			System.out.println("the addition result is: " + addition);
+			op.add(num1, num2);
 		}
 		if(n==2) {
-			int Subtraction = num1-num2;
-			System.out.println("the Subtraction result is: " + Subtraction);
+			op.sub(num1, num2);
 		}
 		if(n==3) {
-			int multiplication = num1*num2;
-			System.out.println("the multiplication result is: " + multiplication);
+			op.mul(num1, num2);
 		}
 		if(n==4) {
-			int division = num1/num2;
-			System.out.println("the division result is: " + division);
+			op.div(num1, num2);
 		}
 		if(n==5) {
-			int mod = num1%num2;
-			System.out.println("the modulus result is: " + mod);
+			op.mod(num1, num2);
 		}
 		if(n==6) {
-			int per = ((num1 + num2)/20)*100;
-			System.out.println("The percentage result is: " + per);
+			op.perc(num1, num2);
 		}
 		else {
 			System.exit(0);
-		}
-	}
-	
-}
-public class calculator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		operation op = new operation();
-		for( ; ; ) {
-			op.operations();
 		}
 	}
 
